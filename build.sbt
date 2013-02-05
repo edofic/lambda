e2b25695f30d38b704f233ccaca979f2f134329d
Andraz Bajt <edofic@gmail.com>
2013-02-05 22:35:36 +0100

    some integration tests for the lanugage

--- build.sbt
+++ build.sbt
@@ -2,6 +2,8 @@ import AssemblyKeys._
 
 scalaVersion := "2.10.0"
 
+libraryDependencies += "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
+
 initialCommands in console := "import com.edofic.lambdas._"
 
 assemblySettings
---
+++ src/test/scala/Integration.scala
@@ -0,0 +1,53 @@
+
+import org.scalatest.FunSuite
+
+import com.edofic.lambdas._
+
+/**
+ * User: andraz
+ * Date: 2/5/13
+ * Time: 9:28 PM
+ */
+class Integration extends FunSuite {
+  def runtime = new Interpreter with Scope with Natives
+
+  def testProgram(name: String, source: String, result: Any) =
+    test(name){
+      assert(runtime.run(Parser(source)) === result)
+    }
+
+  testProgram("simple number", "1", 1)
+
+  testProgram("simple string", "\"hai\"", "hai")
+
+  testProgram("assigments and id's",
+    """
+      |meaning=42
+      |meaning
+    """.stripMargin, 42)
+
+  testProgram("application through native plus",
+    """
+      |plus 1 2
+    """.stripMargin, 3)
+
+  testProgram("two parameter lambda proxy for plus",
+    """
+      |add=\x.\y.plus x y
+      |add 1 2
+    """.stripMargin, 3)
+
+  testProgram("partial application",
+    """
+      |add=\x.\y.plus x y
+      |addOne=add 1
+      |addOne 2
+    """.stripMargin, 3)
+
+  testProgram("lambdas returning lambdas",
+    """
+      |add=\x.\y.plus x y
+      |ax=\x.add x
+      |ax 1 2
+    """.stripMargin, 3)
+}
